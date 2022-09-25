9098b88f3a7192bd1043d5b3cb0b54e6ae8d5afa
Hwassef <hassine.wassef@outloook.com>
2022-09-25 12:56:51 -1100

    add outlined button default theme

---
+++ lib/config/styles/outlined_button_theme.dart
@@ -0,0 +1,61 @@
+import 'package:blog_app/config/colors/app_colors.dart';
+import 'package:blog_app/config/constants/app_constants.dart';
+import 'package:blog_app/config/constants/app_sizes.dart';
+import 'package:flutter/material.dart';
+
+class AppOutlinedButtonTheme {
+  AppOutlinedButtonTheme._();
+  static final OutlinedButtonThemeData outlinedButtonThemeDataLight = OutlinedButtonThemeData(
+    style: _outlinedButtonStyle,
+  );
+
+  static final ButtonStyle _outlinedButtonStyle = ButtonStyle(
+    fixedSize: MaterialStateProperty.resolveWith<Size>(
+      (Set<MaterialState> states) => const Size(
+        double.maxFinite,
+        55,
+      ),
+    ),
+    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+      (states) => AppColors.whiteColor,
+    ),
+    elevation: MaterialStateProperty.resolveWith<double>(
+      (Set<MaterialState> states) => 0.0,
+    ),
+    shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
+      (states) => const RoundedRectangleBorder(
+        borderRadius: AppConstants.mediumBorderRadius,
+      ),
+    ),
+    side: MaterialStateProperty.resolveWith<BorderSide>(
+      (states) {
+        if (states.contains(MaterialState.disabled)) {
+          return const BorderSide(
+            width: AppSizes.size2,
+            color: AppColors.inactiveGreyColorLight,
+          );
+        } else {
+          return const BorderSide(
+            width: 2,
+            color: AppColors.primaryColor,
+          );
+        }
+      },
+    ),
+    textStyle: MaterialStateProperty.resolveWith<TextStyle>((states) {
+      if (states.contains(MaterialState.disabled)) {
+        return const TextStyle(
+          fontFamily: 'Cairo',
+          color: Color(0xFF0A3C5F),
+          fontWeight: FontWeight.w600,
+          fontSize: 14,
+        );
+      }
+      return const TextStyle(
+        color: AppColors.blackColor,
+        fontWeight: FontWeight.w600,
+        fontSize: 14,
+      );
+    }),
+  );
+}
