f778100f4ba65f4a3e4e038eca9e710121b563cc
Hwassef <hassine.wassef@outloook.com>
2022-09-25 12:39:55 -1100

    add sizes constants

---
+++ lib/config/app_sizes.dart
@@ -0,0 +1,24 @@
+class AppSizes {
+  AppSizes._();
+  static const double size2 = 2.0;
+  static const double size4 = 4.0;
+  static const double size6 = 6.0;
+  static const double size8 = 8.0;
+  static const double size10 = 10.0;
+  static const double size12 = 12.0;
+  static const double size14 = 14.0;
+  static const double size16 = 16.0;
+  static const double size18 = 18.0;
+  static const double size20 = 20.0;
+  static const double size22 = 22.0;
+  static const double size24 = 24.0;
+  static const double size26 = 26.0;
+  static const double size28 = 28.0;
+  static const double size30 = 30.0;
+  static const double size32 = 32.0;
+  static const double size34 = 34.0;
+  static const double size36 = 36.0;
+  static const double size38 = 38.0;
+  static const double size40 = 40.0;
+  static const double size42 = 42.0;
+}
