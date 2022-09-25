e207f4ac986a8d8b54a6eac0e03015638d70f656
Hwassef <hassine.wassef@outloook.com>
2022-09-25 12:46:07 -1100

    add colors constants

---
+++ lib/config/colors/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color primaryColor = Color(0xFF3A69D8);
+  static const Color secondaryColor = Color(0xFFE2ECF8);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color deepGreyColor = Color(0xFF8F8F91);
+  static const Color transparentColor = Color(0x00FFFFFF);
+  static const Color inactiveGreyColorLight = Color(0xFFCED8DF);
+  static const Color lightPrimaryColor = Color(0xFFF5F7F9);
+  static const Color darkPinkColor = Color(0xFFEF4970);
+  static const Color lightGreenColor = Color(0xFF14C900);
+  static const Color greyColor = Color(0xFFEEEFF2);
+  static const Color inputHintTextColor = Color(0xFF707070);
+  static const Color shapeGreyColor = Color(0xFFF3F4F7);
+  static const Color purpleBackgroundColor = Color(0xFFAD46CB);
+}
