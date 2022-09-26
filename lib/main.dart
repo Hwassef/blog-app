68c503786fcbf7764053530a89d5a92475d15092
Hwassef <hassine.wassef@outloook.com>
2022-09-25 13:50:32 -1100

    initializing flutter before running app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:blog_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'config/styles/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
