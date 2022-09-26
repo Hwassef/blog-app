f1a9a08e39488996099650e53d903401d770a5c4
Hwassef <hassine.wassef@outloook.com>
2022-09-25 13:26:39 -1100

    export app_input_decoration_theme

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -5,3 +5,4 @@ export 'constants/app_sizes.dart';
 export 'styles/themes/app_text_theme.dart';
 export 'styles/themes/elevated_button_theme.dart';
 export 'styles/themes/outlined_button_theme.dart';
+export 'styles/themes/app_input_decoration_theme.dart';
