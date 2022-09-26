a07fd7ce1f213343e125e2a6f214b10314323514
Hwassef <hassine.wassef@outloook.com>
2022-09-25 13:19:30 -1100

    add default app ThemeData

---
+++ lib/config/styles/app_theme.dart
@@ -0,0 +1,21 @@
+import 'package:blog_app/config/config.dart';
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  AppThemes._();
+
+  static ThemeData defaultAppTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    appBarTheme: const AppBarTheme(color: Colors.blue),
+    bottomAppBarColor: Colors.red,
+    backgroundColor: Colors.white,
+    scaffoldBackgroundColor: Colors.white,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    iconTheme: const IconThemeData(),
+    splashColor: Colors.white,
+    textTheme: AppTextTheme.textThemeLight,
+    disabledColor: AppColors.inactiveGreyColorLight,
+    elevatedButtonTheme: AppElevatedButtonTheme.elevatedButtonThemeDataLight,
+    outlinedButtonTheme: AppOutlinedButtonTheme.outlinedButtonThemeDataLight,
+  );
+}
