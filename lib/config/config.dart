6ab84cae285d416be5a9523eaa99890f6e7ed637
Hwassef <hassine.wassef@outloook.com>
2022-09-25 13:10:27 -1100

    export config files

---
+++ lib/config/config.dart
@@ -0,0 +1,7 @@
+export 'colors/app_colors.dart';
+export 'constants/app_constants.dart';
+export 'constants/app_paddings.dart';
+export 'constants/app_sizes.dart';
+export 'styles/themes/app_text_theme.dart';
+export 'styles/themes/elevated_button_theme.dart';
+export 'styles/themes/outlined_button_theme.dart';
